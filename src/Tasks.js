bb65a31f9fcf4d90a22ac564ebded95a48de3c92
Talha <tkz9696@gmail.com>
2022-05-04 23:16:06 +0500

    Fixed delete function and exports

--- src/Tasks.js
+++ src/Tasks.js
@@ -2,7 +2,7 @@ let tasksArray = JSON.parse(localStorage.getItem('task')) || [];
 const input = document.querySelector('.input');
 const todoList = document.querySelector('.todo-list');
 
-export default class Task {
+class Task {
   constructor(description) {
     this.index = tasksArray.length + 1;
     this.description = description;
@@ -14,9 +14,11 @@ const populateList = (tasksArray) => {
   todoList.innerHTML = '';
   for (let i = 0; i < tasksArray.length; i += 1) {
     const li = document.createElement('li');
-    //  li.id = `${tasksArray[i].index}`;
-    li.innerHTML = `<input type="checkbox" class="checkbox" data_id='${i}' ${tasksArray[i].completed ? 'checked' : ''}><div class="task-text ${tasksArray[i].completed ? 'line' : ''}" contenteditable="true" data_id='${i}'>${tasksArray[i].description}</div>
-        <i class="fas fa-trash-alt delete" data_id='${i}'></i>`;
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox" data_id='${i}' ${tasksArray[i].completed ? 'checked' : ''}>
+      <div class="task-text ${tasksArray[i].completed ? 'line' : ''}" contenteditable="true" data_id='${i}'>${tasksArray[i].description}</div>
+      <i class="fas fa-trash-alt delete" data_id='${i}'></i>
+    `;
     todoList.appendChild(li);
   }
 };
@@ -35,7 +37,7 @@ const updateIndex = () => {
   }
 };
 
-export function addTask() {
+function addTask() {
   if (input.value.trim() !== '') {
     const task = new Task(input.value);
     tasksArray.push(task);
@@ -47,22 +49,22 @@ export function addTask() {
 
 // delete tasks
 todoList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete')) {
-    tasksArray = tasksArray.filter((task) => task.index !== +(e.target.parentNode.id));
+  if (e.target.className.includes('delete')) {
+    tasksArray.splice(e.target.getAttribute('data_id'), 1);
     updateIndex();
     updateLS(tasksArray);
     e.target.parentNode.remove();
   }
 });
 
-export const editTasks = (e) => {
+const editTasks = (e) => {
   if (e.target.className.includes('task-text')) {
     tasksArray[e.target.getAttribute('data_id')].description = e.target.innerText;
     updateLS(tasksArray);
   }
 };
 
-export const check = (e) => {
+const check = (e) => {
   if (e.target.className.includes('checkbox')) {
     const { checked } = e.target;
     const text = e.target.parentNode.querySelector('.task-text');
@@ -76,10 +78,12 @@ export const check = (e) => {
   updateLS(tasksArray);
 };
 
-export const clear = () => {
+const clear = () => {
   tasksArray = tasksArray.filter((task) => (task.completed === false));
   updateLS(tasksArray);
   populateList(tasksArray);
 };
+
+export { addTask, editTasks, check, clear };
 
 populateList(tasksArray);
